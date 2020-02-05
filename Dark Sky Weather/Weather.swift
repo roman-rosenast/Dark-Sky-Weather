e15bc6bd8f6ccf77343271da13e5efb8fa457997
roman-rosenast <rollinroman@gmail.com>
2020-02-05 14:36:34 -0800

    final commit

--- Dark Sky Weather/Weather.swift
+++ Dark Sky Weather/Weather.swift
@@ -1,9 +1,92 @@
 //
 //  Weather.swift
-//  Dark Sky Weather
+//  A file for parsing JSON data from Dark Sky.
+//  Code adapted from original author: Brian Advent
 //
-//  Created by Roman Rosenast on 10/6/19.
+//  Created by Roman Rosenast on 10/5/19.
 //  Copyright © 2019 Roman Rosenast. All rights reserved.
 //
-
 import Foundation
+import CoreLocation
+
+// Weather Model is defined here and all API calls are dealt with in this file so that WeatherTableViewController only deals directly with the information that it needs to display.
+struct Weather {
+    let summary:String
+    let icon:String
+    let day:String
+    let high:Double
+    let low:Double
+    
+    enum SerializationError:Error {
+        case missing(String)
+        case invalid(String, Any)
+    }
+    
+    
+    init(json:[String:Any]) throws {
+        
+        guard let summary = json["summary"] as? String else {throw SerializationError.missing("summary is missing")}
+        
+        guard let icon = json["icon"] as? String else {throw SerializationError.missing("icon is missing")}
+        
+        guard let high = json["temperatureMax"] as? Double else {throw SerializationError.missing("temp high is missing")}
+        
+        guard let low = json["temperatureMin"] as? Double else {throw SerializationError.missing("temp low is missing")}
+
+        guard let time = json["time"] as? Double else {throw SerializationError.missing("time is missing")}
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE"
+        let day = formatter.string(from: Date(timeIntervalSince1970: time))
+        
+        self.summary = summary
+        self.icon = icon
+        self.high = high
+        self.low = low
+        self.day = day
+        
+    }
+    
+    
+    static let basePath = "https://api.darksky.net/forecast/e58d5d056efc18e0c900b516397001a8/"
+    
+    static func forecast (withLocation location:CLLocationCoordinate2D, completion: @escaping ([Weather]?) -> ()) {
+        
+        let url = basePath + "\(location.latitude),\(location.longitude)"
+        let request = URLRequest(url: URL(string: url)!)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data:Data?, response:URLResponse?, error:Error?) in
+            
+            var forecastArray:[Weather] = []
+            
+            if let data = data {
+                
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                        if let dailyForecasts = json["daily"] as? [String:Any] {
+                            if let dailyData = dailyForecasts["data"] as? [[String:Any]] {
+                                for dataPoint in dailyData {
+                                    if let weatherObject = try? Weather(json: dataPoint) {
+                                        forecastArray.append(weatherObject)
+                                    }
+                                }
+                            }
+                        }
+                    
+                    }
+                }catch {
+                    print(error.localizedDescription)
+                }
+                
+                completion(forecastArray)
+                
+            }
+            
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+
+}
--- Dark Sky Weather/WeatherTableViewController.swift
+++ Dark Sky Weather/WeatherTableViewController.swift
@@ -2,89 +2,104 @@
 //  WeatherTableViewController.swift
 //  Dark Sky Weather
 //
-//  Created by Roman Rosenast on 10/6/19.
+//  Created by Roman Rosenast on 10/5/19.
 //  Copyright © 2019 Roman Rosenast. All rights reserved.
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherTableViewController: UITableViewController {
-
+class WeatherTableViewController: UITableViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var Header: UINavigationItem!
+    
+    var locationManager = CLLocationManager()
+    
+//    Model interacts with VC through forecastData array.
+    var forecastData = [Weather]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+//        Default to LA weather
+        updateWeatherForLocation(location: CLLocation(latitude: 34.052235, longitude: -118.2437), defaultLoc: true)
+    }
+    
+//    Updates forecastData array using static function from Weather class then refreshes tableview to show new information.
+    func updateWeatherForLocation (location: CLLocation, defaultLoc: Bool = false) {
+        
+        Header.title = defaultLoc ? "Los Angeles" : "Current Location"
+            
+        Weather.forecast(withLocation: location.coordinate, completion: { (results:[Weather]?) in
+            if let weatherData = results {
+                self.forecastData = weatherData
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        })
+    }
+    
+//    Handle user requesting to see weather report for their current location.
+    @IBAction func getLocation(_ sender: Any) {
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+//    Update tableview with user's inputted location.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations.last
+        updateWeatherForLocation(location: userLocation! )
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return forecastData.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        // Configure the cell...
+        let weatherObject = forecastData[indexPath.row]
+
+        cell.textLabel?.text = "\(weatherObject.day): \(Int(weatherObject.low))° - \(Int(weatherObject.high))°"
+        cell.imageView?.image = UIImage(named: weatherObject.icon)
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+//    Dynamically size tableview cells to fill screen.
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let MinHeight: CGFloat = 80.0
+        let tHeight = tableView.bounds.height
+        
+        let dynamicHeight = tHeight / CGFloat(forecastData.count+1)
+        return dynamicHeight > MinHeight ? dynamicHeight : MinHeight
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+//    Programatically display new page with selected day's summary.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let weatherObject = forecastData[indexPath.row]
+        
+        let summaryVC = UIViewController()
+        let summaryLabel = UILabel(frame: CGRect(x: 0, y: 0, width: summaryVC.view.bounds.width, height: summaryVC.view.bounds.height))
+        summaryLabel.center = CGPoint(x: summaryVC.view.bounds.width/2, y: summaryVC.view.bounds.height/4)
+        summaryLabel.textAlignment = .center
+        summaryLabel.textColor = .white
+        summaryLabel.text = weatherObject.summary
+        
+        summaryVC.view.addSubview(summaryLabel)
+        
+        navigationController?.pushViewController(summaryVC, animated: false)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
